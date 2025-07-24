2a80ea11b48e7635a1e3879d8a3debd9ccefa31c
asume21 <62077744+asume21@users.noreply.github.com>
2025-07-24 01:58:09 -0600

    Fix build error by removing file extensions from index re-exports

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,6 @@
 // Re-export all components to fix build issues
-export { default as AdvancedControls } from './AdvancedControls.jsx';
-export { default as TransportControls } from './TransportControls.jsx';
-export { default as PianoKeys } from './PianoKeys.jsx';
-export { default as ChordProgressionGenerator } from './ChordProgressionGenerator.jsx';
-export { default as usePianoRollLogic } from './usePianoRollLogic.js';
+export { default as AdvancedControls } from './AdvancedControls';
+export { default as TransportControls } from './TransportControls';
+export { default as PianoKeys } from './PianoKeys';
+export { default as ChordProgressionGenerator } from './ChordProgressionGenerator';
+export { default as usePianoRollLogic } from './usePianoRollLogic';
