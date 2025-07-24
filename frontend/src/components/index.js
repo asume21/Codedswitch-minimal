e8b92cd211651b532cac5f3e1bf40348e85bfab2
asume21 <62077744+asume21@users.noreply.github.com>
2025-07-24 02:01:42 -0600

    Fix build error by using direct imports and removing index.js

--- frontend/src/components/index.js
+++
@@ -1,6 +0,0 @@
-// Re-export all components to fix build issues
-export { default as AdvancedControls } from './AdvancedControls';
-export { default as TransportControls } from './TransportControls';
-export { default as PianoKeys } from './PianoKeys';
-export { default as ChordProgressionGenerator } from './ChordProgressionGenerator';
-export { default as usePianoRollLogic } from './usePianoRollLogic';
